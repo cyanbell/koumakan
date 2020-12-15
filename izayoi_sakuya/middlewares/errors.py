401c9c5a22cc185a22ef775df974f953762130ca
shengurun <shengurun@gmail.com>
2020-12-15 12:40:21 +0800

    fix(izayoi_sakuya): fixed server error middleware
    change function `generate_plain_text`, format print error information.s

--- izayoi_sakuya/middlewares/errors.py
+++ izayoi_sakuya/middlewares/errors.py
@@ -8,7 +8,9 @@ class ServerErrorMiddleware(StarletteServerErrorMiddleware):
     # copy from `https://github.com/encode/starlette/pull/1031`
     def generate_plain_text(self, exc: Exception) -> str:
         return "".join(
-            traceback.format_exception(type(exc)),
-            exc,
-            exc.__traceback__
+            traceback.format_exception(
+                type(exc),
+                exc,
+                exc.__traceback__
+            )
         )
